1af3bbf412782cc677b2692626361a578f7d672b
huoyongqiang <Hugo.Huo@osim.com.sg>
2024-03-18 17:53:44 +0800

    Code commit

--- lib/api/healthkit.dart
+++ lib/api/healthkit.dart
@@ -3,10 +3,10 @@ import 'package:healthkit/healthkit_platform_interface.dart';
 final HealthKit healthKit = HealthKit();
 
 class HealthKit {
-  void requireAuth(String provider) =>
+  Future<bool?> requireAuth(String provider) =>
       HealthkitPlatform.instance.requireAuth(provider);
 
-  void cancelAuth(String provider) =>
+  Future<bool?> cancelAuth(String provider) =>
       HealthkitPlatform.instance.cancelAuth(provider);
 
   Future<bool?> testAuth(String provider) =>
