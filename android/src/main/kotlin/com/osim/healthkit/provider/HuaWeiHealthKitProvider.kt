ba59b2899a650785f1181d7c2e6a22254d9282b0
huoyongqiang <Hugo.Huo@osim.com.sg>
2024-03-19 13:36:26 +0800

    Wrapper of HuaWei HealthKit

--- android/src/main/kotlin/com/osim/healthkit/provider/HuaWeiHealthKitProvider.kt
+++ android/src/main/kotlin/com/osim/healthkit/provider/HuaWeiHealthKitProvider.kt
@@ -17,8 +17,13 @@ import com.huawei.hms.hihealth.HuaweiHiHealth
 import com.huawei.hms.hihealth.data.Scopes
 import com.osim.healthkit.IHealthKitLaunchProvider
 import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import kotlin.coroutines.resume
 
 class HuaWeiHealthKitProvider : BaseHealthKitProvider() {
 
@@ -113,7 +118,6 @@ class HuaWeiHealthKitProvider : BaseHealthKitProvider() {
     }
 
     override fun loadData(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
-        val retrievedData = mutableListOf<HiHealthSessionData>()
         testAuth(context) { authorized ->
             Log.d("xxx", "1111111111111111111: $authorized")
             if (authorized) {
@@ -122,54 +126,66 @@ class HuaWeiHealthKitProvider : BaseHealthKitProvider() {
                 val timeout = (params?.get("timeout") as? Double)?.toInt() ?: -1
                 Log.d("xxx", "1111111111111111111: $startTime, $endTime, $timeout")
                 if (startTime >= 0L && endTime >= 0L && timeout >= 0) {
-                    val hiHealthDataQuery = HiHealthDataQuery(
-                        HiHealthSessionType.DATA_SESSION_CORE_SLEEP,
-                        startTime, endTime, HiHealthDataQueryOption(),)
-                    try {
-                        HiHealthDataStore.execQuery(context, hiHealthDataQuery, timeout) { resultCode, data ->
-                            when {
-                                resultCode == HiHealthError.SUCCESS && data is List<*> -> {
-                                    if (data.isNotEmpty()) {
-                                        Log.d("xxx", "------ ${data.size}")
-                                        retrievedData.addAll(data.map { e -> e as HiHealthSessionData })
-                                    } else {
-                                        Log.e("xxx", "Empty sleep data")
-                                    }
-                                }
-                                // 参数错误
-                                resultCode == HiHealthError.PARAM_INVALID ->
-                                    Log.e("xxx", "Param invalid")
-                                // 运动健康版本过低, 不支持此功能
-                                resultCode == HiHealthError.ERR_HEALTH_VERSION_IS_NOT_SUPPORTED ->
-                                    Log.e("xxx", "Health application need to be updated")
-                                // HMS Core版本过低
-                                resultCode == HiHealthError.ERR_HMS_UNAVAILABLE_VERSION ->
-                                    Log.e("xxx", "HMS version is too early")
-                                // 授权失效
-                                resultCode == HiHealthError.ERR_PERMISSION_EXCEPTION ->
-                                    Log.e("xxx", "Permission denied")
-                                // 未同意运动健康隐私协议
-                                resultCode == HiHealthError.ERR_PRIVACY_USER_DENIED ->
-                                    Log.e("xxx", "Privacy user denied")
-                                // 网络异常
-                                resultCode == HiHealthError.ERR_NETWORK ->
-                                    Log.e("xxx", "Network request failed")
-                                // 测试权限的用户数量超过限制
-                                resultCode == HiHealthError.ERR_BETA_SCOPE_EXCEPTION ->
-                                    Log.e("xxx", "Beta scope permission denied")
-                                // 其他错误，建议提示调用失败
-                                else ->
-                                    Log.e("xxx", "Other error, invoking method failed")
-                            }
-                        }
-                    } catch (ex: Exception) {
-                        ex.printStackTrace()
+                    MainScope().launch(Dispatchers.IO) {
+                        loadData(context, cb, startTime, endTime, timeout)
                     }
                 }
             }
         }
-        Log.d("xxx", "+++++++ ${retrievedData.size}")
-        cb?.success(retrievedData)
+    }
+
+    private suspend fun loadData(context: Activity?, cb: MethodChannel.Result?, startTime: Long, endTime: Long, timeout: Int) {
+        val hiHealthDataQuery = HiHealthDataQuery(
+            HiHealthSessionType.DATA_SESSION_CORE_SLEEP,
+            startTime,
+            endTime,
+            HiHealthDataQueryOption(),
+        )
+        cb?.success(suspendCancellableCoroutine {
+            val retrievedData = mutableListOf<HiHealthSessionData>()
+            try {
+                HiHealthDataStore.execQuery(context, hiHealthDataQuery, timeout) { resultCode, data ->
+                    when {
+                        resultCode == HiHealthError.SUCCESS && data is List<*> -> {
+                            if (data.isNotEmpty()) {
+                                Log.d("xxx", "------ ${data.size}")
+                                retrievedData.addAll(data.map { e -> e as HiHealthSessionData })
+                            } else {
+                                Log.e("xxx", "Empty sleep data")
+                            }
+                        }
+                        // 参数错误
+                        resultCode == HiHealthError.PARAM_INVALID ->
+                            Log.e("xxx", "Param invalid")
+                        // 运动健康版本过低, 不支持此功能
+                        resultCode == HiHealthError.ERR_HEALTH_VERSION_IS_NOT_SUPPORTED ->
+                            Log.e("xxx", "Health application need to be updated")
+                        // HMS Core版本过低
+                        resultCode == HiHealthError.ERR_HMS_UNAVAILABLE_VERSION ->
+                            Log.e("xxx", "HMS version is too early")
+                        // 授权失效
+                        resultCode == HiHealthError.ERR_PERMISSION_EXCEPTION ->
+                            Log.e("xxx", "Permission denied")
+                        // 未同意运动健康隐私协议
+                        resultCode == HiHealthError.ERR_PRIVACY_USER_DENIED ->
+                            Log.e("xxx", "Privacy user denied")
+                        // 网络异常
+                        resultCode == HiHealthError.ERR_NETWORK ->
+                            Log.e("xxx", "Network request failed")
+                        // 测试权限的用户数量超过限制
+                        resultCode == HiHealthError.ERR_BETA_SCOPE_EXCEPTION ->
+                            Log.e("xxx", "Beta scope permission denied")
+                        // 其他错误，建议提示调用失败
+                        else ->
+                            Log.e("xxx", "Other error, invoking method failed")
+                    }
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            } finally {
+                it.resume(retrievedData)
+            }
+        })
     }
 
     override fun getVendor(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
