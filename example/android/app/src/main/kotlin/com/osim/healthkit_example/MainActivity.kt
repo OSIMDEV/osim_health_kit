d2d57b7e7047b881679e2c8df653f75817c3dfcb
huoyongqiang <Hugo.Huo@osim.com.sg>
2024-03-19 10:49:31 +0800

    Code commit

--- example/android/app/src/main/kotlin/com/osim/healthkit_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/osim/healthkit_example/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.osim.healthkit_example
 
-import com.osim.healthkit.HealthKitBaseActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import com.osim.healthkit.utils.BetterActivityResult
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: HealthKitBaseActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    private lateinit var _launcher: BetterActivityResult<Intent, ActivityResult>
+
+    val launcher get() = _launcher
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _launcher = BetterActivityResult.registerActivityForResult(this)
+    }
 }
