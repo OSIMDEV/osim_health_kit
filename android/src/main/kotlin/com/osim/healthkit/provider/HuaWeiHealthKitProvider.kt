1000834ae594dfb63a15e9383f69682de07b84c6
huoyongqiang <Hugo.Huo@osim.com.sg>
2024-03-19 10:09:28 +0800

    Wrapper of HuaWei HealthKit

--- android/src/main/kotlin/com/osim/healthkit/provider/HuaWeiHealthKitProvider.kt
+++ android/src/main/kotlin/com/osim/healthkit/provider/HuaWeiHealthKitProvider.kt
@@ -5,6 +5,12 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import com.blankj.utilcode.util.DeviceUtils
+import com.huawei.hihealth.error.HiHealthError
+import com.huawei.hihealthkit.HiHealthDataQuery
+import com.huawei.hihealthkit.HiHealthDataQueryOption
+import com.huawei.hihealthkit.data.HiHealthSessionData
+import com.huawei.hihealthkit.data.store.HiHealthDataStore
+import com.huawei.hihealthkit.data.type.HiHealthSessionType
 import com.huawei.hms.hihealth.HuaweiHiHealth
 import com.huawei.hms.hihealth.data.Scopes
 import com.osim.healthkit.HealthKitBaseActivity
@@ -60,13 +66,111 @@ class HuaWeiHealthKitProvider : BaseHealthKitProvider() {
         (context as? HealthKitBaseActivity)?.apply {
             val intent = HuaweiHiHealth.getSettingController(this).requestAuthorizationIntent(scopes, true)
             launcher.launch(intent) {
-                val result = HuaweiHiHealth.getSettingController(this).parseHealthKitAuthResultFromIntent(it.data)
-                cb?.success(result?.isSuccess ?: false)
+                var success = false
+                try {
+                    val result = HuaweiHiHealth.getSettingController(this).parseHealthKitAuthResultFromIntent(it.data)
+                    success = result?.isSuccess ?: false
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+                cb?.success(success)
             }
+        } ?: cb?.success(false)
+    }
+
+    override fun testAuth(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
+        testAuth(context) {
+            cb?.success(it)
         }
     }
 
+    private fun testAuth(context: Activity?, cb: (Boolean)->Unit) {
+        context?.apply {
+            HuaweiHiHealth.getSettingController(this)?.healthAppAuthorization
+                ?.addOnFailureListener {
+                    cb(false)
+                }
+                ?.addOnSuccessListener {
+                    cb(it)
+                } ?: cb(false)
+        } ?: cb(false)
+    }
+
+    override fun cancelAuth(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
+        (context as? Context)?.apply {
+            HuaweiHiHealth.getSettingController(this)?.healthAppAuthorization
+                ?.addOnFailureListener {
+                    cb?.success(false)
+                }
+                ?.addOnSuccessListener {
+                    cb?.success(it)
+                } ?: cb?.success(false)
+        } ?: cb?.success(false)
+    }
+
+    override fun loadData(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
+        val retrievedData = mutableListOf<HiHealthSessionData>()
+        testAuth(context) { authorized ->
+            if (authorized) {
+                val startTime = (params?.get("startTime") as? Long) ?: -1L
+                val endTime = (params?.get("endTime") as? Long) ?: -1L
+                val timeout = (params?.get("timeout") as? Int) ?: -1
+                if (startTime >= 0L && endTime >= 0L && timeout >= 0L) {
+                    val hiHealthDataQuery = HiHealthDataQuery(
+                        HiHealthSessionType.DATA_SESSION_CORE_SLEEP,
+                        startTime, endTime, HiHealthDataQueryOption(),)
+                    try {
+                        HiHealthDataStore.execQuery(context, hiHealthDataQuery, timeout) { resultCode, data ->
+                            when {
+                                resultCode == HiHealthError.SUCCESS && data is List<*> -> {
+                                    if (data.isNotEmpty()) {
+                                        retrievedData.addAll(data.map { e -> e as HiHealthSessionData })
+                                    } else {
+                                        Log.e(TAG, "Empty sleep data")
+                                    }
+                                }
+                                // 参数错误
+                                resultCode == HiHealthError.PARAM_INVALID ->
+                                    Log.e(TAG, "Param invalid")
+                                // 运动健康版本过低, 不支持此功能
+                                resultCode == HiHealthError.ERR_HEALTH_VERSION_IS_NOT_SUPPORTED ->
+                                    Log.e(TAG, "Health application need to be updated")
+                                // HMS Core版本过低
+                                resultCode == HiHealthError.ERR_HMS_UNAVAILABLE_VERSION ->
+                                    Log.e(TAG, "HMS version is too early")
+                                // 授权失效
+                                resultCode == HiHealthError.ERR_PERMISSION_EXCEPTION ->
+                                    Log.e(TAG, "Permission denied")
+                                // 未同意运动健康隐私协议
+                                resultCode == HiHealthError.ERR_PRIVACY_USER_DENIED ->
+                                    Log.e(TAG, "Privacy user denied")
+                                // 网络异常
+                                resultCode == HiHealthError.ERR_NETWORK ->
+                                    Log.e(TAG, "Network request failed")
+                                // 测试权限的用户数量超过限制
+                                resultCode == HiHealthError.ERR_BETA_SCOPE_EXCEPTION ->
+                                    Log.e(TAG, "Beta scope permission denied")
+                                // 其他错误，建议提示调用失败
+                                else ->
+                                    Log.e(TAG, "Other error, invoking method failed")
+                            }
+                        }
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
+                }
+            }
+        }
+        cb?.success(retrievedData)
+    }
+
     override fun getVendor(context: Activity?, cb: MethodChannel.Result?, params: Map<*, *>?) {
-        cb?.success(DeviceUtils.getManufacturer())
+        cb?.success(
+            try {
+                DeviceUtils.getManufacturer()
+            } catch (ex: Exception) {
+                ""
+            }
+        )
     }
 }
